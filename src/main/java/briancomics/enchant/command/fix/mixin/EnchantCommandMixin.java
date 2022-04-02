42d483883aee0ce47512d8b36ea962e3195c792d
BrianComics <72044529+BrianComics@users.noreply.github.com>
2022-04-02 17:03:21 -0400

    Add files via upload

---
+++ src/main/java/briancomics/enchant/command/fix/mixin/EnchantCommandMixin.java
@@ -0,0 +1,15 @@
+package briancomics.enchant.command.fix.mixin;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.server.command.EnchantCommand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(EnchantCommand.class)
+public class EnchantCommandMixin {
+	@Redirect(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/Enchantment;getMaxLevel()I"))
+	private static int bypassLevelCheck (Enchantment enchantment) {
+		return Integer.MAX_VALUE;
+	}
+}
